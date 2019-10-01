bfb5a7e67a40e587b7c94521834c09e698be5d38
Steven Taylor <steven.taylor788@googlemail.com>
2019-10-01 17:43:05 +0100

    Adds rush label function

--- faster/utils.py
+++ faster/utils.py
@@ -7,3 +7,6 @@ def getAllTeams(games):
         teamDict[team] = count
         count = count + 1
     return teamDict
+
+def getRushSeries(games):
+    return ((games["FTHG"] > 0) & (games["FTAG"] > 0)).map(lambda x: 1 if x else 0)
